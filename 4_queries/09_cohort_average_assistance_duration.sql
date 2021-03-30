c09bdc8924d70f7cedcf37bd0881a24eb35212df
Sam <sribas@gmail.com>
2021-03-30 17:35:37 +0000

    Week05/Day02 - Cohort Average Assistance Duration

---
+++ 4_queries/09_cohort_average_assistance_duration.sql
@@ -0,0 +1,18 @@
+SELECT AVG(total_duration) FROM (SELECT cohorts.name AS cohort, SUM(completed_at - started_at) AS total_duration
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY total_duration) AS averate_total_duration;
+
+/*
+SELECT avg (total_duration) as average_total_duration
+FROM (
+  SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = student_id
+  JOIN cohorts on cohorts.id = cohort_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) as total_durations;
+*/
