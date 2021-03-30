759490aa71f2eed5882883ef39395acad8d295da
Sam <sribas@gmail.com>
2021-03-30 16:16:32 +0000

    Week05/Day02 - Cohort With Longest Assistances

---
+++ 4_queries/6_cohort_with_longest_assistances.sql
@@ -0,0 +1,17 @@
+SELECT cohorts.name, AVG(completed_at - started_at) AS average_assistance_time
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
+
+/*
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
+*/
