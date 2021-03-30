91f858c651c5252c8b7137cbd795003931663213
Sam <sribas@gmail.com>
2021-03-30 19:33:29 +0000

    Week05/Day02 - Assistance Requests Data

--- 4_queries/03_assistance_requests_data.sql
+++ 4_queries/03_assistance_requests_data.sql
@@ -1,9 +1,15 @@
-SELECT teachers.name AS teacher, students.name AS student, assignments.name, (completed_at - started_at) AS duration
+SELECT teachers.name AS teacher, students.name As student, assignments.name AS assignment, (assistance_requests.completed_at - assistance_requests.started_at) AS duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+LEFT JOIN assignments ON assignments.id = assistance_requests.assignment_id
+ORDER BY duration
+
+/*
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
-JOIN assignments ON assignments.id = assistance_requests.assignment_id
-GROUP BY students.name, teachers.name, assignments.name
-ORDER BY duration
-
-
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
+*/
