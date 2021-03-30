a923a928555cd307775b9aea6db827bd89375105
Sam <sribas@gmail.com>
2021-03-30 13:53:21 +0000

    Week05/Day02 - BootcampX Assistance Requests

--- join.sql
+++ join.sql
@@ -2,5 +2,5 @@ SELECT students.name as student_name, cohorts.name as cohort_name, cohorts.start
 FROM students
 JOIN cohorts
 ON cohorts.id = cohort_id
-WHERE students.start_date <> cohorts.start_date
+WHERE students.start_date != cohorts.start_date
 ORDER BY cohorts.start_date;
