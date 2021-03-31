c026a7a083a0736de6ac2c43074b67cebe723114
Sam <sribas@gmail.com>
2021-03-31 20:29:45 +0000

    Week05/Day03 - Node Postgres - Querying The Database with Javascript

---
+++ bootcamp_app/students.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const cohort = process.argv[2];
+const limit = process.argv[3];
+
+const pool = new Pool( {
+  user:     'vagrant',
+  password: '123',
+  host:     'localhost',
+  database: 'bootcampx',
+  port:     5432
+});
+
+pool.query(
+  `SELECT students.id, students.name, cohorts.name AS cohort
+  FROM students 
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  LIMIT 5;`)
+  // .then(res => {
+  //   console.log(res.rows);
+  //   console.log("Number of rows:", res.rowCount);
+  // })
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id ${user.id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => console.log('query error', err.stack));
+
